bb46ee0d5f96f61db29021a10d70ae352b49f1c4
Angel Sánchez <angelsanchez058@gmail.com>
2020-02-25 16:00:08 -0500

    gitignore file added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,7 +4,9 @@ export default class Sidebar extends Component {
   render() {
     return (
       <div className="col-3">
-        <p>Server info</p>
+        <h5>Server info</h5>
+        <p>Space used: 34.4MB</p>
+        <p>Space left: 2.04TB</p>
       </div>
     );
   }
